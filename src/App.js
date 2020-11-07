97fa73800408a405ee314c655c10dba1e9180a64
Mateusz Bogacz <mateusz@martcode.pl>
2020-11-07 18:41:46 +0100

    Init.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './index.css';
+import { Form, FormFormik } from './components/Form';
+
+function App() {
+    return (
+        <div>
+            <Form />
+            <FormFormik />
+        </div>
+    );
+}
+
+export default App;
